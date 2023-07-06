7d986d47b43e3dc0686f84318aa19e97f5f2cd3d
1F10220197 <s1f102201975@iniad.org>
2023-07-06 14:04:47 +0900

    エラーの解消

--- todo/views.py
+++ todo/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
-# Create your views here.
 
+
+# Create your views here.
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
