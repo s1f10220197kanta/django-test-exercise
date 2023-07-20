8750fc721175419c0146f6440acf07d57d21b73e
1F10220199 <s1f102201997@iniad.org>
2023-07-20 13:52:25 +0900

    終了機能の追加

--- todo/views.py
+++ todo/views.py
@@ -56,3 +56,11 @@ def delete(request, task_id):
         raise Http404("Task does not exist")
     task.delete()
     return redirect(index)
+
+def close(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.save()
+    return redirect(index)
